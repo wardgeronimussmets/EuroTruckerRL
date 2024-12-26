fix time calculation

src/reinforcement_learning/ets2_rl_environment.py
@@ -11,7 +11,7 @@ MAX_TIME_WITHOUT_PROGRESS_SECONDS = 60
 
 class ETS2RLEnvironment(gym.Env):
 
-    def __init__(self):
+    def __init__(self, skip_initialize=False):
         super().__init__()
 
         '''action space:
@@ -34,13 +34,12 @@ class ETS2RLEnvironment(gym.Env):
             {
                 "screen": gym.spaces.Box(low=0, high=255,
                                                 shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8),
-                "max_speed": gym.spaces.Box(low=0, high=TRUCK_MAX_DETECTABLED_SPEED, shape=(1,), dtype=np.uint8) #assuming a max speed
+                "max_speed": gym.spaces.Discrete(TRUCK_MAX_DETECTABLED_SPEED + 1)  # assuming max speed is inclusive
             }
-
         )
         
         self.step_interpreter = StepInterpreter()
-        self.ets2_interactor = ETS2Interactor()
+        self.ets2_interactor = ETS2Interactor(skip_initialize=skip_initialize)
         self.current_time_to_travel = 0
         self.last_improvement_time = math.inf
 
@@ -114,15 +113,16 @@ class ETS2RLEnvironment(gym.Env):
         if self.current_time_to_travel - current_time_to_travel > 0:
             #making progress
             self.last_improvement_time = current_time
-            return False
         else:
             #no progress
             if current_time - self.last_improvement_time > MAX_TIME_WITHOUT_PROGRESS_SECONDS:
                 return True
+        return False
             
 
     def reset(self, seed=None, options=None):
-        self.ets2_interactor.start_new_job()
+        # self.ets2_interactor.start_new_job()
+        # wsme
         current_time_to_travel, max_speed, current_speed, info_title, penalty_score, whole_screen_resized = self.step_interpreter.calculate_values()
         self.current_time_to_travel = current_time_to_travel
         return (self._get_obs(whole_screen_resized, self._rescale_max_speed_if_necesarry(max_speed)), 
@@ -156,4 +156,4 @@ class ETS2RLEnvironment(gym.Env):
 
 if __name__ == "__main__":
     print("Testing environment")
-    stable_baselines3.common.env_checker.check_env(ETS2RLEnvironment(), warn=True, skip_render_check=True)
+    stable_baselines3.common.env_checker.check_env(ETS2RLEnvironment(skip_initialize=False), warn=True, skip_render_check=True)

src/reinforcement_learning/step_interpreter.py
@@ -19,14 +19,14 @@ class StepInterpreter:
         images = self.screen_grabber.get_images()
         screenshotTime = time.time()
         current_time_to_travel = self._get_current_time_to_travel(images[0])
-        max_speed = self._get_max_speed(images[1])
-        current_speed = self._get_current_speed(images[2])
-        info_title = self._read_from_info_title_region(images[3])
-        whole_screen_resized = images[4]
+        max_speed = self._get_max_speed(images[self.screen_grabber.get_max_speed_image_index()])
+        current_speed = self._get_current_speed(images[self.screen_grabber.get_current_speed_image_index()])
+        info_title = self._read_from_info_title_region(images[self.screen_grabber.get_info_title_image_index()])
+        whole_screen_resized = np.transpose(images[self.screen_grabber.get_whole_screen_resized_image_index()], (2, 0, 1))
         penalty_score = 0
         #todo wsme: need to add behaviour for if you want to take ferry, rest, ... best to add in the environment itself NOT HERE
         if info_title == ImageSimilarityMatch.INFO and screenshotTime - self.last_info_detected_time > INFO_DETECTED_TIMEOUT_SECONDS:
-            penalty_score = self._extract_penalty_score_from_extra_information_from_gps(images[4])
+            penalty_score = self._extract_penalty_score_from_extra_information_from_gps(images[self.screen_grabber.get_gps_info_image_index()])
             self.last_info_detected_time = screenshotTime
         return current_time_to_travel, max_speed, current_speed, info_title, penalty_score, whole_screen_resized
     
@@ -47,21 +47,21 @@ class StepInterpreter:
         currentInfoString = pytesseract.image_to_string(image=info_image)
         splitInfo = currentInfoString.split(",")
         if len(splitInfo) > 2:
-            timeInfo = splitInfo[2]
+            timeInfo = splitInfo[2] # rest is information about the time of day and which day
             return self._extract_time_in_min_from_game_time(timeInfo)
+        else:
+            return 0
 
     def _extract_time_in_min_from_game_time(self, gameTimeString):
-        currentNum = 0
+        gameTimeString = gameTimeString.replace("\n", "").replace("\t", "")
         timeInMins = 0
         for chars in gameTimeString.split(" "):
-            if chars.isdigit():
-                currentNum += int(chars)
-            elif chars == "h":
-                timeInMins += currentNum * 60
-                currentNum = 0
-            elif chars == "min":
-                timeInMins += currentNum
-                currentNum = 0
+            if chars:
+                if len(chars) > 1 and chars[-1] == "h":
+                    timeInMins += int(chars[:-1]) * 60
+                elif len(chars) > 3 and chars[-3:] == "min":
+                    timeInMins += int(chars[:-3])
+        return timeInMins
                 
 
     def _get_max_speed(self, max_speed):